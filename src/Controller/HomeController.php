3f1808fc25459eb6d377a8deba56e8bcbd4b4f7d
Jocelyn Bonvard <jbonvard@aol.fr>
2023-06-23 20:51:12 +0200

    check responsive

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ImageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function homepage(): Response
+    public function homepage(ImageRepository $repoImage, EntityManagerInterface $em): Response
     {   
+        $images = $repoImage->findBy([], null, 5);
+        
         return $this->render('home/index.html.twig', [
+            'images' => $images
         ]);
     }
 }
