e5175ea216b7c00e1b5e3f53bc7951c9ece2c3e2
Jocelyn Bonvard <jbonvard@aol.fr>
2023-06-24 17:15:58 +0200

    done +

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\ImageRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function homepage(ImageRepository $repoImage, EntityManagerInterface $em): Response
+    public function homepage(ImageRepository $repoImage): Response
     {   
         $images = $repoImage->findBy([], null, 5);
         
